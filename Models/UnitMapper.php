f58f6e7b21a530e312a768c185ea7d10cfde8479
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-10-24 17:52:24 +0200

    cs and phpstan fixes

--- Models/UnitMapper.php
+++ Models/UnitMapper.php
@@ -46,7 +46,7 @@ final class UnitMapper extends DataMapperAbstract
     /**
      * Has one relation.
      *
-     * @var array<string, array{mapper:string, self:string, by?:string, column?:string}>
+     * @var array<string, array{mapper:string, external:string, by?:string, column?:string, conditional?:bool}>
      * @since 1.0.0
      */
     protected static array $ownsOne = [
@@ -59,7 +59,7 @@ final class UnitMapper extends DataMapperAbstract
     /**
      * Belongs to.
      *
-     * @var array<string, array{mapper:string, self:string}>
+     * @var array<string, array{mapper:string, external:string}>
      * @since 1.0.0
      */
     protected static array $belongsTo = [
