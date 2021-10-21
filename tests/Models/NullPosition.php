32969fd244bb9e89a3ab0563b2e22d9addcb0fc4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:16:06 +0200

    more tests and make classes final

---
+++ tests/Models/NullPosition.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\Organization\tests\Models;
+
+use Modules\Organization\Models\NullPosition;
+
+/**
+ * @internal
+ */
+final class Null extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Organization\Models\NullPosition
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\Organization\Models\Position', new NullPosition());
+    }
+
+    /**
+     * @covers Modules\Organization\Models\NullPosition
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullPosition(2);
+        self::assertEquals(2, $null->getId());
+    }
+}
